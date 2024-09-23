4b774af0a65f7d49a31e2364f7ee435fba9c494d
Qishuai <ferdinandzhong@gmail.com>
2024-09-23 21:45:58 +0800

    update for the py.sh

--- scripts/py.sh
+++ scripts/py.sh
@@ -3,6 +3,6 @@ set -e
 
 export CONDA_ENV="workshop-env"
 echo "Start creating Conda Environment ${CONDA_ENV}"
-conda create -n CONDA_ENV python=3.9.18
-conda activate workshop-env
+conda create -n ${CONDA_ENV} python=3.9.18
+conda activate ${CONDA_ENV}
 echo "Conda Environment ${CONDA_ENV} has been created"
