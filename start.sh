bb9072d857fedaa89d4313b0cb16db8d8df790fa
Qishuai <ferdinandzhong@gmail.com>
2024-09-23 23:07:41 +0800

    remove comments

--- start.sh
+++ start.sh
@@ -6,23 +6,17 @@ cd scripts || { echo "scripts directory not found! Exiting."; exit 1; }
 
 # echo "Start setting up the environment"
 
-# # Iterate over script files and execute them
-# for cmd in base.sh py.sh fish.sh; do
-#     if [ -f "$cmd" ]; then
-#         echo "Running $cmd..."
-#         bash "$cmd"
-#     else
-#         echo "Warning: $cmd not found, skipping..."
-#     fi
-# done
+# Iterate over script files and execute them
+for cmd in base.sh py.sh fish.sh; do
+    if [ -f "$cmd" ]; then
+        echo "Running $cmd..."
+        bash "$cmd"
+    else
+        echo "Warning: $cmd not found, skipping..."
+    fi
+done
 
 # Return to the parent directory
 cd ..
-
-source ~/.bashrc
-
-# Activate the Conda base environment
-echo "Activating Conda base environment..."
-conda activate base
 
 echo "Environment setup complete! Enjoy :-)"
