1297b82207011475b5974cc8f55c64843ef5a728
Qishuai <ferdinandzhong@gmail.com>
2024-09-23 23:06:15 +0800

    temp comment

--- start.sh
+++ start.sh
@@ -4,17 +4,17 @@ set -e
 # Move into the scripts directory
 cd scripts || { echo "scripts directory not found! Exiting."; exit 1; }
 
-echo "Start setting up the environment"
+# echo "Start setting up the environment"
 
-# Iterate over script files and execute them
-for cmd in base.sh py.sh fish.sh; do
-    if [ -f "$cmd" ]; then
-        echo "Running $cmd..."
-        bash "$cmd"
-    else
-        echo "Warning: $cmd not found, skipping..."
-    fi
-done
+# # Iterate over script files and execute them
+# for cmd in base.sh py.sh fish.sh; do
+#     if [ -f "$cmd" ]; then
+#         echo "Running $cmd..."
+#         bash "$cmd"
+#     else
+#         echo "Warning: $cmd not found, skipping..."
+#     fi
+# done
 
 # Return to the parent directory
 cd ..
