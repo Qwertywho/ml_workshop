7e1cc1cf41ad4b37fe3138f4445e15043050d0a1
Qishuai <ferdinandzhong@gmail.com>
2024-09-23 22:34:16 +0800

    update script

--- scripts/conda.sh
+++ scripts/conda.sh
@@ -12,4 +12,7 @@ ${HOME}/miniconda3/bin/conda init
 . ${HOME}/.bashrc
 
 # related libraries
+${HOME}/miniconda3/bin/conda install -y -c conda-forge bat fd-find
 pip install httpie flake8 black isort autoflake ipython
+mkdir -p ${HOME}/.config
+cp flake8 ${HOME}/.config/
