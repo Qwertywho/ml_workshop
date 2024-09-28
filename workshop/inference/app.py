3f6dca086b44c114b461d85b222f6680a8db290c
Qishuai <ferdinandzhong@gmail.com>
2024-09-28 14:58:27 +0800

    update serving script

--- workshop/inference/app.py
+++ workshop/inference/app.py
@@ -140,4 +140,4 @@ if __name__ == "__main__":
     )
 
     # Run the FastAPI app from the class
-    uvicorn.run(model_api.app, host="0.0.0.0", port=args.port, reload=True)
+    uvicorn.run("__main__.app", host="0.0.0.0", port=args.port, reload=True)
